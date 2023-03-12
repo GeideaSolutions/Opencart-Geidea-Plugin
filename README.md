74c5fa44393f6b078107d41a0e089d95f19150d5
Mohamed-Amr <39967199+Mohamed-Amr@users.noreply.github.com>
2023-03-12 10:37:26 +0200

    opencart 4.x upload

--- README.md
+++
@@ -1,9 +0,0 @@
-# Opencart Geidea payment gateway module
-
-## Installation
-
-1 - download the folder and rename it to 'geidea-opencart.ocmod'
-
-2 - Zip the folder
-
-3 - Upload it as an extension
--- upload/admin/language/en-gb/extension/payment/geidea_bank_card.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'GeideaPay';
-
-// Text
-$_['text_geidea_bank_card'] = '<a target="_BLANK" href="https://geidea.net/egypt/en/"><img src="view/image/payment/logo.png" alt="Geidea Website" title="Geidea Website" style="border: 1px solid #EEEEEE;height:30px;" /></a>';
